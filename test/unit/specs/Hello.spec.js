ba53d67b9c45ef6b18f97bb57879a346f2a9f29d
Adam Woodbury <adamwoodbury@gmail.com>
2017-10-25 09:26:32 -0700

    use vue test utils

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,11 +1,10 @@
-import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld'
+import { mount } from 'vue-test-utils'
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+    const wrapper = mount(HelloWorld)
+
+    expect(wrapper.find('.hello h1').text()).to.equal('Welcome to Your Vue.js App')
   })
 })
